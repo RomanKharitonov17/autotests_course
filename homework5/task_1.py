22b751e8f032bc8e41644c8b6ca742cbba596741
RomanKharitonov17 <132955670+RomanKharitonov17@users.noreply.github.com>
2023-05-19 16:12:37 +0300

    Описание параметров.py

--- homework5/task_1.py
+++ homework5/task_1.py
@@ -7,7 +7,10 @@
 
 
 def letter_stat(our_str):
-    """Подсчитывает количество символов в строке"""
+    """Подсчитывает количество символов в строке
+       :param: our_str - входящая строка
+       :return: letters_dict - словарь. Ключи - это символы строки. Значения - количество повторений символа в строке.
+    """
     letters_dict = {item: 0 for item in our_str}
     for i in our_str:
         letters_dict[i] += 1
