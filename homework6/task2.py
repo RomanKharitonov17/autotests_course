aef2917064dc12f779d2b28f7006fa64984f39c6
RomanKharitonov17 <kharitonovroman17@inbox.ru>
2023-05-23 19:55:02 +0300

    Задание 2. Нелокальные изменения

--- homework6/task2.py
+++ homework6/task2.py
@@ -5,11 +5,22 @@
 # local_function должна изменить значение переменной msg на значение 2
 
 def global_function():
-  msg = 1
-  def local_function():
-  # Здесь нужно написать код
-   
-  return msg
+    """ Глобальная функция вызывает local_function()
+        :param: msg - число, переменная из объемлющей области видимости для local_function()
+    """
+    msg = 1
+
+    def local_function():
+        """ Меняет переменную msg
+                :param: msg - переменная из global_function()
+                :return: msg
+            """
+        nonlocal msg
+        msg = 2
+
+    local_function()
+    return msg
+
 
 assert global_function() == 2, 'Значение переменной msg должно быть равно 2'
 print('Все ок')
