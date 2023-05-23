545e29fe79e92dcb0f2fafa2f1dc1edcb3e2a6ab
RomanKharitonov17 <kharitonovroman17@inbox.ru>
2023-05-23 19:31:19 +0300

    Задание 1. Изменение глобальных переменных.

--- homework6/task1.py
+++ homework6/task1.py
@@ -12,9 +12,19 @@ string = 'Hello'
 
 
 def global_changes():
-    # Здесь нужно написать код
+    """Изменяет глобальные переменные.
+           :params: number - глобальная переменная, число;
+                    string - глобальная переменная, строка.
+           :return: number, string - измененные функцией глобальные переменные.
+    """
+    global number
+    number = 5
+    global string
+    string = 'Hello, dear friend'
+    return number, string
 
 
+global_changes()
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 assert number == 5, 'Переменная number должна иметь значение 5'
