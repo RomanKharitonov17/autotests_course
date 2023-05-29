aa7ab1447cb27bf3653001651686f0694c16b1e4
RomanKharitonov17 <kharitonovroman17@inbox.ru>
2023-05-29 23:51:53 +0300

    Задание 3. PublicTransport

--- homework7/task_3.py
+++ homework7/task_3.py
@@ -24,7 +24,50 @@
 # У класса должно быть СВОЙСТВО how_long, которое вычисляет время за прохождение маршрута по формуле max_speed/(4*path)
 
 # Здесь пишем код
+class PublicTransport:
 
+    def __init__(self, brand, engine_power, year, color, max_speed):
+        self.brand = brand
+        self._engine_power = engine_power
+        self.year = year
+        self.color = color
+        self.max_speed = max_speed
+
+    @property
+    def info(self):
+        print(self.brand, self.color, self.year, self._engine_power)
+
+
+class Bus(PublicTransport):
+
+    def __init__(self, brand, engine_power, year, color, max_speed, passengers, park, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.passengers = passengers
+        self.__park = park
+        self._fare = fare
+
+    @property
+    def park(self):
+        return self.__park
+
+    @park.setter
+    def park(self, park):
+        assert 9999 > park >= 1000
+        self.__park = park
+
+
+class Tram(PublicTransport):
+
+    def __init__(self, brand, engine_power, year, color, max_speed, route, path, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.__route = route
+        self.path = path
+        self._fare = fare
+
+    @property
+    def how_long(self):
+        how_long = self.max_speed / (4 * self.path)
+        return how_long
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 transport = PublicTransport('Автомобиль', 500, 2040, 'Фиолетовый', 300)
