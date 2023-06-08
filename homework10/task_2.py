6471768e47d8d7ec79346fc40130aa1b9fd636b9
RomanKharitonov17 <kharitonovroman17@inbox.ru>
2023-06-08 23:34:56 +0300

    Задание 2.

--- homework10/task_2.py
+++ homework10/task_2.py
@@ -10,8 +10,30 @@ import pytest
 
 
 def all_division(*arg1):
-
     division = arg1[0]
     for i in arg1[1:]:
         division /= i
     return division
+
+
+def test_division_by_zero_error():
+    with pytest.raises(ZeroDivisionError):
+        all_division(2, 0)
+
+
+def test_division_no_args_error():
+    with pytest.raises(IndexError):
+        all_division()
+
+
+def test_division_letter_error():
+    with pytest.raises(TypeError):
+        all_division('a', 'b')
+
+@pytest.mark.smoke
+def test_positive():
+    assert all_division(4, 2) == 2
+
+
+def test_positive2():
+    assert all_division(6, 3, 2) == 1
