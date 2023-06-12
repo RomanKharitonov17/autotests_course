297b3ae07e93e0485f08b0545fb95863aa2ead4a
RomanKharitonov17 <kharitonovroman17@inbox.ru>
2023-06-12 23:51:19 +0300

    Задание 1. Декомпозиция логики генератора. Добавление маркера в pytest.ini

--- homework10/task_1.py
+++ homework10/task_1.py
@@ -16,19 +16,27 @@
 import random
 
 
-def generate_random_name():
-    def generate_random_word(word):
-        letters = 'abcdefghijklmnopqrstuvwxyz'
-        for i in range(random.randint(1, 15)):
-            word += (random.choice(letters))
-        return word
+def generate_random_word():
+    """
+    Создает случайное слово латинских букв от 1 до 15 символов
+    :return: word - сгенерированное слово
+    """
+    word = ''
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(random.randint(1, 15)):
+        word += (random.choice(letters))
+    return word
 
+
+def generate_random_name():
+    """
+    Генерирует два слова из латинских букв от 1 до 15 символов, разделенных пробелами
+    """
     while True:
-        word_1 = generate_random_word('')
-        word_2 = generate_random_word('')
+        word_1 = generate_random_word()
+        word_2 = generate_random_word()
         yield f"{word_1} {word_2}"
 
 
-# Здесь пишем код
 gen = generate_random_name()
 print(next(gen))
